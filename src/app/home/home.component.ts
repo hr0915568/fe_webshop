4c3028d7f646c273147e073820e22453802ab901
mbrambrouwer <mbrambrouwer@gmail.com>
2018-01-15 14:24:39 +0100

    homepage update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import {HttpClient} from '@angular/common/http'
+import { ProductService }  from '../_services/product.service';
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/switchMap';
+
+import { Jsonp,RequestOptions } from '@angular/http';
+import {ActivatedRoute, ParamMap, Params, Router} from "@angular/router";
+import {Product} from '../_models/product';
+import {CartService} from '../_services/cart.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +16,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  product: Product = new Product();
+  constructor(private route: ActivatedRoute,
+    private productService: ProductService,
+    private http: HttpClient) { }
 
   ngOnInit() {
+      this.productService.getProduct(11)
+      .subscribe((product) => this.product = product);
   }
 
 }
