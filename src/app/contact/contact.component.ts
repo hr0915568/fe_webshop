0f43c8ade30eb4c9a6bc3386f7d9a8e739d07c96
Jinxi Cheng <0915568@hr.nl>
2018-01-15 14:07:06 +0100

    contact works now

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {AlertService} from '../_services/alert.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  name: string;
+  email: string;
+  message: string;
+  phone: string;
+
+  constructor(private http: HttpClient, private alertService: AlertService) { }
 
   ngOnInit() {
   }
 
+  sendMessage()
+  {
+    const body = new HttpParams()
+      .set('email', this.email)
+      .set('message', this.message)
+      .set('phone', this.phone)
+      .set('name', this.name);
+
+    return this.http.post('http://api.hrwebshop.tk/contact', body.toString(),
+      {
+        responseType: 'text',
+        withCredentials: true,
+        headers: new HttpHeaders()
+          .set('Content-type', 'application/x-www-form-urlencoded')
+      }
+    ).subscribe(
+      response => {
+        //clear object
+        this.alertService.success("Your message is sent.");
+        this.email = null;
+        this.name = null;
+        this.message = null;
+      }
+    );
+  }
 }
